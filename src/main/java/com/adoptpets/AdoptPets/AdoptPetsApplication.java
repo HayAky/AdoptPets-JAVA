d23a674273651c24a55683cc496677420184d6b5
Andres Castiblanco <andrescastibla@hotmail.com>
2025-11-16 13:37:14 -0500

    init

---
+++ src/main/java/com/adoptpets/AdoptPets/AdoptPetsApplication.java
@@ -0,0 +1,13 @@
+package com.adoptpets.AdoptPets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptPetsApplication.class, args);
+	}
+
+}
